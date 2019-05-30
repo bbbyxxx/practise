8f1f25ca23f304ea4c94c47b350185598dbaeb9c
bbbyxxx <1397766942@qq.com>
2019-05-30 15:48:05 +0800

    leetcode

--- 数组--两个数组的交集II.cpp
+++ 数组--两个数组的交集II.cpp
@@ -1,3 +1,18 @@
+两个数组的交集 II
+给定两个数组，编写一个函数来计算它们的交集。
+
+示例 1:
+
+输入: nums1 = [1,2,2,1], nums2 = [2,2]
+输出: [2,2]
+示例 2:
+
+输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出: [4,9]
+说明：
+
+输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
+我们可以不考虑输出结果的顺序。
 #include<bits/stdc++.h>
 using namespace std;
 int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize){
